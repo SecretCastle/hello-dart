00f3a2b5960b93fb4ddd4d0df7cc8cbd06bffdce
SecretCastle <chenfangzhong@hotmail.com>
2019-08-16 10:32:59 +0800

    test alarm clock

--- index.dart
+++ index.dart
@@ -3,14 +3,13 @@ import 'Clock.dart';
 /**
  * 定义测试时间戳
  */
-final clock_time = '09:57';
+final clock_time = '10:35';
 
 alarmClockDo() {
   print('do do do ~~~ do do do ~~~');
 }
 
 main() {
-  // final alarmClock = new AlarmClock(1);
-  // alarmClock.initAlarmClock(clock_time, alarmClockDo);
-  print('hello github action');
+  final alarmClock = new AlarmClock(1);
+  alarmClock.initAlarmClock(clock_time, alarmClockDo);
 }
